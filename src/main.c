66ac58d7df4123ce4e603c669bc13d09b9fb11ff
Strugglemeat <36766617+Strugglemeat@users.noreply.github.com>
2022-09-19 21:22:28 +0800

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -67,9 +67,6 @@ int main()
         if(collisionTest(&P2, BOTTOM)==false){}
         else pieceIntoBoard(&P2);
 
-        if(P1.board[4][3]!=0)P1.flag_status=toppedOut;
-        if(P2.board[4][3]!=0)P2.flag_status=toppedOut;
-
         if(P1.flag_checkmatches==true)checkMatches(&P1);
         if(P2.flag_checkmatches==true)checkMatches(&P2);
 
@@ -120,8 +117,8 @@ void printDebug()
     //sprintf(debug_string,"P1 %d", P1.flag_status);
     //VDP_drawText(debug_string,32,1);
 
-    sprintf(debug_string,"P1y:%d", P1.yPosition);
-    VDP_drawText(debug_string,2,4);
+    //sprintf(debug_string,"P1y:%d", P1.yPosition);
+    //VDP_drawText(debug_string,2,4);
 
     //sprintf(debug_string,"P1x:%d", P1.xPosition);
     //VDP_drawText(debug_string,1,2);
@@ -131,6 +128,11 @@ void printDebug()
         sprintf(debug_string,"P1 topped out");
         VDP_drawText(debug_string,1,2);
     }
+    if(P2.flag_status==toppedOut)
+    {
+        sprintf(debug_string,"P2 topped out");
+        VDP_drawText(debug_string,27,2);
+    }
 
     //sprintf(debug_string,"P2:%d", P2.flag_status);
     //VDP_drawText(debug_string,25,5);
@@ -138,23 +140,6 @@ void printDebug()
     //sprintf(debug_string,"dInc:%d", debugIncrementer);
     //VDP_drawText(debug_string,25,5);
 
-    //sprintf(debug_string,"P1:%d,%d,%d", P1.fallingPiece[0],P1.fallingPiece[1],P1.fallingPiece[2]);
-    //VDP_drawText(debug_string,2,1);
-    //sprintf(debug_string,"Pre:%d,%d,%d", P1.nextPiece[0],P1.nextPiece[1],P1.nextPiece[2]);
-    //VDP_drawText(debug_string,1,2);
-/*
-    if(P1.flag_status==needPiece)
-    {
-        for (u8 printBoardX=1;printBoardX<maxX+1;printBoardX++)
-        {
-            for (u8 printBoardY=6;printBoardY<maxY+1;printBoardY++)
-            {
-                sprintf(debug_string,"%d",P1.board[printBoardX][printBoardY]);
-                VDP_drawText(debug_string,printBoardX,printBoardY-3);
-            }
-        }
-    }
-*/
 /*
     //if(P1.flag_destroy==true)
     //if(1)
@@ -236,45 +221,48 @@ bool collisionTest(Player* player, u8 direction)
 
 void handleInput(Player* player, u16 buttons)
 {
-    if(buttons & BUTTON_LEFT && player->moveDelay==0 && collisionTest(player, LEFT)==FALSE)
+    if(player->flag_status!=toppedOut)
     {
-        player->xPosition--;
-        player->moveDelay=MOVE_DELAY_AMOUNT;
-        player->spriteX-=TILESIZE;
-    }
-    else if(buttons & BUTTON_RIGHT && player->moveDelay==0 && collisionTest(player, RIGHT)==FALSE)
-    {
-        player->xPosition++;
-        player->moveDelay=MOVE_DELAY_AMOUNT;
-        player->spriteX+=TILESIZE;
-    }
+        if(buttons & BUTTON_LEFT && player->moveDelay==0 && collisionTest(player, LEFT)==FALSE)
+        {
+            player->xPosition--;
+            player->moveDelay=MOVE_DELAY_AMOUNT;
+            player->spriteX-=TILESIZE;
+        }
+        else if(buttons & BUTTON_RIGHT && player->moveDelay==0 && collisionTest(player, RIGHT)==FALSE)
+        {
+            player->xPosition++;
+            player->moveDelay=MOVE_DELAY_AMOUNT;
+            player->spriteX+=TILESIZE;
+        }
 
-    if (buttons & BUTTON_DOWN && player->fallDelay==0 && collisionTest(player, BOTTOM)==FALSE)
-    {
-        //player->fallingIncrement+=2;
-        //player->spriteY+=2;
+        if (buttons & BUTTON_DOWN && player->fallDelay==0 && collisionTest(player, BOTTOM)==FALSE)
+        {
+            //player->fallingIncrement+=2;
+            //player->spriteY+=2;
 
-        player->yPosition++;
-        player->spriteY+=TILESIZE;
+            player->yPosition++;
+            player->spriteY+=TILESIZE;
 
-        player->fallDelay=FALL_DELAY_AMOUNT;
+            player->fallDelay=FALL_DELAY_AMOUNT;
+        }
+
+        if (buttons & BUTTON_B && player->rotateDelay==0 && player->has_let_go_B==true)
+        {
+            doRotate(player, DOWN);
+            player->rotateDelay=ROTATE_DELAY_AMOUNT;
+            player->has_let_go_B=false;
+        }
+        else if (buttons & BUTTON_A && player->rotateDelay==0 && player->has_let_go_A==true)
+        {
+            doRotate(player, UP);
+            player->rotateDelay=ROTATE_DELAY_AMOUNT;
+            player->has_let_go_A=false;
+        }
+
+        if(!(buttons & BUTTON_A))player->has_let_go_A=true;
+        if(!(buttons & BUTTON_B))player->has_let_go_B=true;
     }
-
-    if (buttons & BUTTON_B && player->rotateDelay==0 && player->has_let_go_B==true)
-    {
-        doRotate(player, DOWN);
-        player->rotateDelay=ROTATE_DELAY_AMOUNT;
-        player->has_let_go_B=false;
-    }
-    else if (buttons & BUTTON_A && player->rotateDelay==0 && player->has_let_go_A==true)
-    {
-        doRotate(player, UP);
-        player->rotateDelay=ROTATE_DELAY_AMOUNT;
-        player->has_let_go_A=false;
-    }
-
-    if(!(buttons & BUTTON_A))player->has_let_go_A=true;
-    if(!(buttons & BUTTON_B))player->has_let_go_B=true;
 
     if(buttons & BUTTON_C)//debug
     {
@@ -307,8 +295,9 @@ void pieceIntoBoard(Player* player)
     player->drawEndX=player->xPosition+1;
     player->drawEndY=player->yPosition+1;
 
-    player->flag_status=needPiece;
-
+    if(player->board[4][3]==0)player->flag_status=needPiece;
+    else player->flag_status=toppedOut;
+    
     player->flag_checkmatches=true;
 
     player->flag_redraw=true;
@@ -328,11 +317,13 @@ void checkMatches(Player* player)
                 {
                     connectionAmount=2;
                     connectionColor=player->board[checkX][checkY];
+
                     for (u8 advance=checkX+2;advance<maxX+1;advance++)
                     {
                         if(player->board[advance][checkY]==connectionColor)connectionAmount++;
                         else if(player->board[advance][checkY]!=connectionColor)break;
                     }
+
                     if(connectionAmount>=3)
                     {
                         //sprintf(debug_string,"matched %d laterally starting at %d",connectionAmount,checkX);
@@ -348,14 +339,73 @@ void checkMatches(Player* player)
                 }
                 if(player->board[checkX][checkY]==player->board[checkX][checkY-1])//match vertically 2 tiles
                 {
+                    connectionAmount=2;
+                    connectionColor=player->board[checkX][checkY];
 
+                    //sprintf(debug_string,"init vert match at %d,%d",checkX,checkY);
+                    //VDP_drawText(debug_string,1,1);
+
+                    for (u8 advance=checkY-2;advance>0;advance--)
+                    {
+                        if(player->board[checkX][advance]==connectionColor)connectionAmount++;
+                        else if(player->board[checkX][advance]!=connectionColor)break;
+                    }
+
+                    if(connectionAmount>=3)
+                    {
+                        //sprintf(debug_string,"matched %d vertically starting at %d",connectionAmount,checkY);
+                        //VDP_drawText(debug_string,1,1);
+                        
+                        for (u8 yAddDestructionQueue=0;yAddDestructionQueue<connectionAmount;yAddDestructionQueue++)
+                        {
+                            player->boardDestructionQueue[checkX][checkY-yAddDestructionQueue]=true;
+                        }
+
+                        player->flag_destroy=true;
+                    }
                 }
                 if(player->board[checkX][checkY]==player->board[checkX+1][checkY-1])//match diagonally up 2 tiles
                 {
+                    //sprintf(debug_string,"init diagUp match at %d,%d",checkX,checkY);
+                    //VDP_drawText(debug_string,1,1);
 
+                    connectionAmount=2;
+                    connectionColor=player->board[checkX][checkY];
+                    /*
+                    u8 incrementer=2;
+
+                    for (u8 advance=checkY-2;(advance>0 && ((checkX+incrementer)<(maxX+1)));advance--)
+                    //for (u8 advance=checkY-1;advance>0;advance--)
+                    {
+                        if(player->board[checkX+incrementer][advance]==connectionColor)connectionAmount++;
+                        else if(player->board[checkX+incrementer][advance]!=connectionColor)break;
+                        incrementer++;
+                    }
+
+                    if(connectionAmount>=3)
+                    {
+                        //sprintf(debug_string,"matched %d diagUp starting at %d,%d",connectionAmount,checkX,checkY);
+                        //VDP_drawText(debug_string,1,1);
+                        incrementer=0;
+                        
+                        for (u8 yAddDestructionQueue=0;yAddDestructionQueue<connectionAmount;yAddDestructionQueue++)
+                        {
+                            player->boardDestructionQueue[checkX+incrementer][checkY-incrementer]=true;
+                        
+                            //sprintf(debug_string,"diagUp clr %d,%d",checkX+xIncrement,checkY-yAddDestructionQueue);
+                            //VDP_drawText(debug_string,26,1+xIncrement);
+
+                            incrementer++;
+                        }
+
+                        player->flag_destroy=true;
+                    }
+                */
                 }
                 if(player->board[checkX][checkY]==player->board[checkX+1][checkY+1])//match diagonally down 2 tiles
                 {
+                    connectionAmount=2;
+                    connectionColor=player->board[checkX][checkY];
 
                 }
                 else if(player->board[checkX][checkY]==0)break;//empty tile, leave
@@ -369,7 +419,7 @@ void checkMatches(Player* player)
 void processDestroy(Player* player)
 {
     u8 howManyDestroyed=0;
-    u8 debug_firstX=0;
+    //u8 debug_firstX=0;
 
     for (u8 destroyX=1;destroyX<maxX+1;destroyX++)
     {
@@ -381,7 +431,7 @@ void processDestroy(Player* player)
                 player->boardDestructionQueue[destroyX][destroyY]=false;
                 howManyDestroyed++;
 
-                if(debug_firstX==0)debug_firstX=destroyX;
+                //if(debug_firstX==0)debug_firstX=destroyX;
             }
         }
     }
@@ -389,41 +439,33 @@ void processDestroy(Player* player)
     player->flag_destroy=false;
     player->flag_gravity=true;
 
-/*
-    if(howManyDestroyed>=3)
-    {
-        sprintf(debug_string,"destroyed %d starting at %d",howManyDestroyed,debug_firstX);
-        VDP_drawText(debug_string,1,2);
-    }
-*/
+
+    //if(howManyDestroyed>=3)
+    //{
+        //sprintf(debug_string,"destroyed %d starting at %d",howManyDestroyed,debug_firstX);
+        //VDP_drawText(debug_string,1,1);
+    //}
+
 }
 
 void processGravity(Player* player)
 {
     //u8 debug_howMuchGravity=0;
-    u8 debugbreakYpos=3;
+    //u8 debugbreakYpos=3;
 
     for (u8 gravityX=1;gravityX<maxX+1;gravityX++)
     {
-        //for (u8 gravityY=maxY-1;gravityY>0;gravityY--)//don't need to start at maxY+1 cus there's no gravity for bottom tiles
-        for (u8 gravityY=maxY-1;gravityY>0;gravityY--)//#define maxY 17
+        for (u8 gravityY=maxY;gravityY>0;gravityY--)//#define maxY 17
         {
-            if(player->board[gravityX][gravityY]==0 && player->board[gravityX][gravityY-1]==0)
-                {
-                    sprintf(debug_string,"g brk@ %d,%d",gravityX,gravityY);
-                    VDP_drawText(debug_string,28,debugbreakYpos);
-                    debugbreakYpos++;
-                    break;
-                }
-            else if (player->board[gravityX][gravityY]!=0 && player->board[gravityX][gravityY+1]==0)
-            //if (player->board[gravityX][gravityY]!=0 && player->board[gravityX][gravityY+1]==0)
+            if (player->board[gravityX][gravityY]==0 && player->board[gravityX][gravityY-1]!=0)
             {
-                player->board[gravityX][gravityY+1]=player->board[gravityX][gravityY];
-                player->board[gravityX][gravityY]=0;
-                
+                player->board[gravityX][gravityY]=player->board[gravityX][gravityY-1];
+                player->board[gravityX][gravityY-1]=0;
+
+                gravityY=maxY+1;
+
                 //debug_howMuchGravity++;
             }
-            //else if(player->board[gravityX][gravityY]==0)break;
         }
     }
 
